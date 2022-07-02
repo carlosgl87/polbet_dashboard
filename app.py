3ce1123969dea55d4f6a157c10b9e7cc3eedac44
Carlos Gamero <cgamero@unique-yanbal.com>
2022-07-02 12:27:04 -0500

    add google analytics

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from pymongo import MongoClient
 import ssl
 from bson.objectid import ObjectId
 from datetime import date, timedelta
+from google.oauth2 import service_account
+from apiclient.discovery import build
 
 pd.set_option('display.float_format', lambda x: '%.2f' % x)
 
@@ -42,6 +44,115 @@ mon_retiros = 0
 df_retiros = pd.DataFrame(columns=['Usuario','Monto','Fecha'])
 df_depositos = pd.DataFrame(columns=['Usuario','Monto','Fecha'])
 
+######################
+##### Analytics ######
+######################
+
+your_view_id = '269046440'
+ga_keys = 'betpol-21eeb29ef8a9.json'
+
+def format_summary(response):
+    try:
+        # create row index
+        try: 
+            row_index_names = response['reports'][0]['columnHeader']['dimensions']
+            row_index = [ element['dimensions'] for element in response['reports'][0]['data']['rows'] ]
+            row_index_named = pd.MultiIndex.from_arrays(np.transpose(np.array(row_index)), 
+                                                        names = np.array(row_index_names))
+        except:
+            row_index_named = None
+        
+        # extract column names
+        summary_column_names = [item['name'] for item in response['reports'][0]
+                                ['columnHeader']['metricHeader']['metricHeaderEntries']]
+    
+        # extract table values
+        summary_values = [element['metrics'][0]['values'] for element in response['reports'][0]['data']['rows']]
+    
+        # combine. I used type 'float' because default is object, and as far as I know, all values are numeric
+        df = pd.DataFrame(data = np.array(summary_values), 
+                          index = row_index_named, 
+                          columns = summary_column_names).astype('float')
+    
+    except:
+        df = pd.DataFrame()
+        
+    return df
+
+def format_pivot(response):
+    try:
+        # extract table values
+        pivot_values = [item['metrics'][0]['pivotValueRegions'][0]['values'] for item in response['reports'][0]
+                        ['data']['rows']]
+        
+        # create column index
+        top_header = [item['dimensionValues'] for item in response['reports'][0]
+                      ['columnHeader']['metricHeader']['pivotHeaders'][0]['pivotHeaderEntries']]
+        column_metrics = [item['metric']['name'] for item in response['reports'][0]
+                          ['columnHeader']['metricHeader']['pivotHeaders'][0]['pivotHeaderEntries']]
+        array = np.concatenate((np.array(top_header),
+                                np.array(column_metrics).reshape((len(column_metrics),1))), 
+                               axis = 1)
+        column_index = pd.MultiIndex.from_arrays(np.transpose(array))
+        
+        # create row index
+        try:
+            row_index_names = response['reports'][0]['columnHeader']['dimensions']
+            row_index = [ element['dimensions'] for element in response['reports'][0]['data']['rows'] ]
+            row_index_named = pd.MultiIndex.from_arrays(np.transpose(np.array(row_index)), 
+                                                        names = np.array(row_index_names))
+        except: 
+            row_index_named = None
+        # combine into a dataframe
+        df = pd.DataFrame(data = np.array(pivot_values), 
+                          index = row_index_named, 
+                          columns = column_index).astype('float')
+    except:
+        df = pd.DataFrame()
+    return df
+
+def format_report(response):
+    summary = format_summary(response)
+    pivot = format_pivot(response)
+    if pivot.columns.nlevels == 2:
+        summary.columns = [['']*len(summary.columns), summary.columns]
+    
+    return(pd.concat([summary, pivot], axis = 1))
+
+def run_report(body, credentials_file):
+    #Create service credentials
+    credentials = service_account.Credentials.from_service_account_file(credentials_file, 
+                                scopes = ['https://www.googleapis.com/auth/analytics.readonly'])
+    #Create a service object
+    service = build('analyticsreporting', 'v4', credentials=credentials)
+    
+    #Get GA data
+    response = service.reports().batchGet(body=body).execute()
+    
+    return(format_report(response))
+
+vista = {
+  "reportRequests":
+  [
+    {
+      "viewId": your_view_id,
+      "dateRanges": [{"startDate": "2022-05-01", "endDate": "today"}],
+      "metrics": [{"expression": "ga:users"},
+                  {"expression": "ga:sessions"},
+                  {"expression": "ga:newUsers"}],
+       "dimensions": [
+            {"name": "ga:pagePath"},
+            {"name": "ga:date"}
+            ]
+    }
+  ]
+}
+
+ga_report = run_report(vista, ga_keys)
+ga_report = ga_report.reset_index()
+ga_report['ga:date'] = pd.to_datetime(ga_report['ga:date'],format='%Y%m%d')
+ga_report['indicador_contest_page'] = ga_report['ga:pagePath'].str.contains('/contest-detail-page')
+ga_report['id_contest'] = ga_report['ga:pagePath'].apply(lambda x: x[x.find('-page/') + 6: 1000])
 
 ######################
 ######## Page ########
